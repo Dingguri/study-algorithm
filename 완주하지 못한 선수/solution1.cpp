b55497210f09d167f628b558555f2efecad433e1
PARK SEUNG MIN <min62156347@gmail.com>
2019-12-24 23:50:29 +0900

    완주하지 못한 선수 : https://www.welcomekakao.com/learn/courses/30/lessons/42576
    unordered_multiset 을 활용한 풀이로 수정

--- 완주하지 못한 선수/solution1.cpp
+++ 완주하지 못한 선수/solution1.cpp
@@ -1,35 +1,24 @@
 // 완주하지 못한 선수 : https://www.welcomekakao.com/learn/courses/30/lessons/42576
-// # 해시
+// #해시
 
 #include <string>
 #include <vector>
-#include <unordered_map>
-#include <stdexcept>
+#include <unordered_set>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    unordered_map<string, int> completionMap;
-    completionMap.reserve(completion.size());
-
-    for(const string& name : completion) {
-        if(!completionMap.insert(pair<string,int>(name, 1)).second) {   // insert 실패 == 같은 key값 존재
-            ++completionMap[name]; 
-        }        
-    }
-
-    string answer = "";
+    unordered_multiset<string> participantMultSet;
+    participantMultSet.reserve(participant.size());
 
     for(const string& name : participant) {
-        try{
-            if(--completionMap.at(name) <= 0) {
-                completionMap.erase(name);
-            }
-        }
-        catch (const out_of_range& oor) {
-            answer = name;
-        }
+        participantMultSet.insert(name);
+    }
+
+    for(const string& name : completion) {
+        auto it = participantMultSet.find(name);
+        participantMultSet.erase(it);
     }
     
-    return answer;
+    return *participantMultSet.begin();
 }
