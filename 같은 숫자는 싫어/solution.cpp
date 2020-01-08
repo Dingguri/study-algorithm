7131e650b5bb48012a7a9a894818cff2006fc763
PARK SEUNG MIN <min62156347@gmail.com>
2020-01-08 22:59:32 +0900

    같은 숫자는 싫어 : https://www.welcomekakao.com/learn/courses/30/lessons/12906
    used unique

--- 같은 숫자는 싫어/solution.cpp
+++ 같은 숫자는 싫어/solution.cpp
@@ -1,17 +1,11 @@
 // 같은 숫자는 싫어 : https://www.welcomekakao.com/learn/courses/30/lessons/12906
 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    vector<int> answer;
-    int pre = -1;
-    for(const auto& value : arr){
-        if(pre != value){
-            pre = value;
-            answer.push_back(value);
-        }
-    }
-    return answer;
+    arr.erase(unique(arr.begin(), arr.end()),arr.end());
+    return arr;
 }
