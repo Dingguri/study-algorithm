286129d37fe8d79be40a70317133efc22721410d
PARK SEUNG MIN <min62156347@gmail.com>
2020-01-15 21:55:56 +0900

    문자열을 정수로 바꾸기 : https://www.welcomekakao.com/learn/courses/30/lessons/12925

---
+++ 문자열을 정수로 바꾸기/solution.cpp
@@ -0,0 +1,11 @@
+// 문자열을 정수로 바꾸기 : https://www.welcomekakao.com/learn/courses/30/lessons/12925
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = stoi(s);
+    return answer;
+}
