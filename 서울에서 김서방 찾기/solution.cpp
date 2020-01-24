d2527d21d9cf8fc0c0b3c298ac5052ea818b88d2
PARK SEUNG MIN <min62156347@gmail.com>
2020-01-24 21:27:59 +0900

    서울에서 김서방 찾기 : https://programmers.co.kr/learn/courses/30/lessons/12919
    find, distance

--- 서울에서 김서방 찾기/solution.cpp
+++ 서울에서 김서방 찾기/solution.cpp
@@ -2,16 +2,13 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<string> seoul) {
-    string answer = "";
-    for(size_t i = 0; i < seoul.size(); ++i){
-        if(!seoul[i].compare("Kim")) {
-            answer = "김서방은 " + to_string(i) + "에 있다";
-            break;
-        }
-    }
-    return answer;
+	auto it = find(seoul.cbegin(), seoul.cend(), "Kim");
+	int idx = distance(seoul.cbegin(), it);
+	string answer = "김서방은 " + to_string(idx) + "에 있다";
+	return answer;
 }
