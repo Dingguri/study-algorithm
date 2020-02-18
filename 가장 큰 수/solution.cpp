a8de68e86bcabc8ec477ccb9a2d5fb0d15d4d935
PARK SEUNG MIN <min62156347@gmail.com>
2020-02-18 23:42:08 +0900

    가장 큰 수 : https://programmers.co.kr/learn/courses/30/lessons/42746

--- 가장 큰 수/solution.cpp
+++ 가장 큰 수/solution.cpp
@@ -1,4 +1,4 @@
-// 가장 큰 수 : https://programmers.co.kr/learn/courses/30/lessons/42746#
+// 가장 큰 수 : https://programmers.co.kr/learn/courses/30/lessons/42746
 // #정렬
 
 #include <string>
@@ -10,24 +10,15 @@ using namespace std;
 string solution(vector<int> numbers) {
     vector<string> strNumbers;
     strNumbers.reserve(numbers.size());
-    for(int number : numbers) {
+    for(int number : numbers)
         strNumbers.push_back(to_string(number));
-    }
 
-    sort(strNumbers.rbegin(), strNumbers.rend(), [](string a, string b) {
-        bool ret = false;
-        size_t size = a.length() > b.length() ? a.length() : b.length();
-        for(size_t i = 0; i <= size; ++i) { // [12, 121], [21, 212] 을 해결하기 위해 i <= size
-            if(a[i % a.length()] != b[i % b.length()]) {
-                ret = a[i % a.length()] < b[i % b.length()];
-                break;
-            }
-        }
-        return ret;
+    sort(strNumbers.begin(), strNumbers.end(), [](string a, string b) {
+        return a + b > b + a;
     });
 
-    string answer = "";
-    if(strNumbers[0][0] == '0') {
+    string answer = "";    
+    if(strNumbers[0] == "0") {
         answer = "0";
     }
     else {
